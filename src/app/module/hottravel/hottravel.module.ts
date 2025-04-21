f3bf1a07ffc901ad6da143fbe9cd6316c5fbd08e
heshanu <heshanu97@gmail.com>
2025-04-22 02:30:09 +0530

    not clear map

--- src/app/module/hottravel/hottravel.module.ts
+++ src/app/module/hottravel/hottravel.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HottravelRoutingModule } from './hottravel-routing.module';
 import { HottravellistComponent } from './hottravellist/hottravellist.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MapComponent } from '../../shared/map/map.component';
 
 @NgModule({
   declarations: [
     HottravellistComponent,
+    MapComponent,
+   
   ],
   imports: [
     CommonModule,
     HottravelRoutingModule,
     FormsModule,
     ReactiveFormsModule
-  ]
+  ],
+  exports:[MapComponent]
 })
 export class HottravelModule { }
