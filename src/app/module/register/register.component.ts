60e08fc8498e057fc157f7b944ac00f751e38ad6
heshanu <heshanu97@gmail.com>
2025-06-05 11:31:53 +0530

    implemented

--- src/app/module/register/register.component.ts
+++ src/app/module/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthServiceCall } from '../../service/auth.service';
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-register',
@@ -14,14 +15,14 @@ export class RegisterComponent implements OnInit{
 
     registerForm!: FormGroup;
     show!: boolean;
-    
+
     errorMessage!: string;
-  
+
     constructor(private fb: FormBuilder,
       private route: ActivatedRoute,private router: Router,
       private http: HttpClient,private authService:AuthServiceCall
     ){}
-  
+
     ngOnInit(): void {
       this.registerForm = this.fb.group({
         username: ['', Validators.required],
@@ -29,13 +30,19 @@ export class RegisterComponent implements OnInit{
         confirmPassword: ['', [Validators.required, Validators.minLength(3)]]
       });
     }
-      
+
       onSubmit() {
         if (this.registerForm.valid) {
         const { username, password} = this.registerForm.value;
-  
-         console.log(username,password);
-                
+
+          console.log(username, password);
+                                Swal.fire({
+            title: 'Action Successfully executed',
+            text: 'Customer registered successfully',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          });
+
           this.authService.register(username,password).subscribe({
             next: (textResponse) => {
               if (textResponse == "User registered successfully") {
@@ -43,12 +50,18 @@ export class RegisterComponent implements OnInit{
               }
             },
             error: (err) => {
+                                    Swal.fire({
+                title: 'Action Successfully executed',
+                text: 'Unable to registered successfully',
+                icon: 'error',
+                confirmButtonText: 'OK'
+              });
               console.error('Error:', err);
               this.errorMessage = "Register failed";
             }
           });
-  
-         
+
+
         }
       }
 
