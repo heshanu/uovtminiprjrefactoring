03c32f2332ef5950c85b4f33a5a386019c198610
heshanu <heshanu97@gmail.com>
2025-06-18 23:46:46 +0530

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 
 export const environment={
   production:false,
-  misrelAPIKEY:'',
-  photoServiceAPI:'',
-  TWILIO_ACCOUNT_SID:'',
-    TWILIO_AUTH_TOKEN:'',
-  TWILIO_PHONE_NUMBER:''
+  misrelAPIKEY:'H1PSVLETk1g6H0SnZKCQcY8exIeoyKuB',
+  photoServiceAPI:'9BjmTonkuZo6jlKHv3phLYvD3s23TXfzb94kL2QDZXc9wF1QfRAISX65',
+  TWILIO_ACCOUNT_SID:'AC10dddf108e283281b5fec3e6fefc9b1b',
+    TWILIO_AUTH_TOKEN:'a473b0de640133aa82c12af6fdd3a749',
+  TWILIO_PHONE_NUMBER:'+14155238886'
 }
