c851388496dad372cd2e2eeca2258d12dc20f780
heshanu <heshanu97@gmail.com>
2025-06-20 22:47:41 +0530

    fixed

--- src/app/module/location/location.module.ts
+++ src/app/module/location/location.module.ts
@@ -5,9 +5,7 @@ import { LocationRoutingModule } from './location-routing.module';
 import { LocationComponent } from './location.component';
 import { MaterialModule } from '../../shared/materialModule';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { SharedcompoentModule } from '../sharedcompoent/sharedcompoent.module';
-
 @NgModule({
   declarations: [
     LocationComponent
