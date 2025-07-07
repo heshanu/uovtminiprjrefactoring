4d5d797d552972f092168c74456bb3ae02e5b2fc
Heshan Umayanga <38934645+heshanu@users.noreply.github.com>
2025-07-08 00:43:54 +0530

    updated update customer endpoint (#147)

--- src/app/shared/beverageconfirm/beverageconfirm.component.ts
+++ src/app/shared/beverageconfirm/beverageconfirm.component.ts
@@ -6,42 +6,42 @@ import { AppState } from '../../app.reducer';
 import { addBeverage } from '../../store/orders/orders.actions';
 
 @Component({
-    selector: 'app-beverageconfirm',
-    templateUrl: './beverageconfirm.component.html',
-    styleUrl: './beverageconfirm.component.css',
-    standalone: false
+  selector: 'app-beverageconfirm',
+  templateUrl: './beverageconfirm.component.html',
+  styleUrl: './beverageconfirm.component.css',
+  standalone: false
 })
 export class BeverageconfirmComponent {
 
-   readonly dialog = inject(MatDialog);
- 
-  packages:any[]=[
-        {name:"small" ,value:" LKR 500"},
-        {name:"Standard",value:"LKR 1500"},
-        {name:"medium ",value:"LKR 2000"},
-        {name:"Large ",value: "LKR 4000"},
-        {name:"GrandPa",value:" LKR 2000"},
-        {name:"Monthly Special",value:"LKR 15000"}
-    ]
- 
-   constructor(
-     public dialogRef: MatDialogRef<BeverageconfirmComponent>,
-     @Inject(MAT_DIALOG_DATA) public data: any,
-     private store:Store<AppState>
-   ) {}
+  readonly dialog = inject(MatDialog);
 
- 
-   bookPackage(pkg:any,enterAnimationDuration: string, exitAnimationDuration: string) {
+  packages: any[] = [
+    { name: "small", value: " LKR 500" },
+    { name: "Standard", value: "LKR 1500" },
+    { name: "medium ", value: "LKR 2000" },
+    { name: "Large ", value: "LKR 4000" },
+    { name: "GrandPa", value: " LKR 2000" },
+    { name: "Monthly Special", value: "LKR 15000" }
+  ]
+
+  constructor(
+    public dialogRef: MatDialogRef<BeverageconfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private store: Store<AppState>
+  ) { }
+
+
+  bookPackage(pkg: any, enterAnimationDuration: string, exitAnimationDuration: string) {
     console.log(pkg);
-    pkg.price=100;
-    pkg.quantity=1;
-    this.store.dispatch(addBeverage({order:pkg}));
+    pkg.price = 100;
+    pkg.quantity = 1;
+    this.store.dispatch(addBeverage({ order: pkg }));
     this.dialog.open(ConfirmationModalComponent, {
-     width: '250px',
-     enterAnimationDuration,
-     exitAnimationDuration,
-   });
-    
-   }
-  
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+  }
+
 }
