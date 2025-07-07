892c36345082d26081eb28d8d4322fd6d2d8b127
Heshan Umayanga <38934645+heshanu@users.noreply.github.com>
2025-07-07 20:59:29 +0530

    Pre dev (#146)
    * updated twilio token
    
    * fixed
    
    * fixed env issues
    
    * fixed (#129)
    
    * fixed (#131)
    
    * fixed cors
    
    * fiexed (#134)
    
    * fixed minor changes in customers Dashboard (#136)
    
    * fixed by adding text color according to themes (#138)
    
    * fixed styles in header
    
    * fixed (#140)
    
    * fixed (#141)
    
    remove chat feature
    
    * fixed (#143)
    
    * updated delete customer endpoint

--- src/app/compoent/customers-dash-board/customers-dash-board.component.ts
+++ src/app/compoent/customers-dash-board/customers-dash-board.component.ts
@@ -2,26 +2,28 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CustomerdetailsInterface } from '../../model/customerDetailsInterface';
 import { ActivatedRoute, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { setCustomer} from '../../store/customers/customer-id.actions';
+import { setCustomer } from '../../store/customers/customer-id.actions';
 import { finalize, Observable, Subscription } from 'rxjs';
 import { getCustomerDetail, selectCustomerId } from '../../store/customers/customer.selectors';
 import { AppState } from '../../app.reducer';
 import { CustomerdetailsService } from '../../service/customerdetails.service';
 import { SpinnerService } from '../../service/spinner.service';
 import { CustomerState } from '../../store/customers/customer.status';
+import Swal from 'sweetalert2';
+
 @Component({
-    selector: 'app-customers-dash-board',
-    templateUrl: './customers-dash-board.component.html',
-    styleUrl: './customers-dash-board.component.css',
-    standalone: false
+  selector: 'app-customers-dash-board',
+  templateUrl: './customers-dash-board.component.html',
+  styleUrl: './customers-dash-board.component.css',
+  standalone: false
 })
 
-export class CustomersDashBoardComponent implements OnInit,OnDestroy{
+export class CustomersDashBoardComponent implements OnInit, OnDestroy {
 
-  customerService=inject(CustomerdetailsService);
+  customerService = inject(CustomerdetailsService);
 
-  customerDetails$!: Observable<CustomerState|undefined>;
-  customerId!:string|undefined;
+  customerDetails$!: Observable<CustomerState | undefined>;
+  customerId!: string | undefined;
   private subscription!: Subscription;
 
   isLoading$!: Observable<boolean>;
@@ -31,27 +33,28 @@ export class CustomersDashBoardComponent implements OnInit,OnDestroy{
     message: ''
   };
 
-  customersList:CustomerdetailsInterface[]=[];
+  customersList: CustomerdetailsInterface[] = [];
 
-  constructor(private route:Router,private activeRouter:ActivatedRoute,
-    private store: Store<AppState>,private spinnerService:SpinnerService,private customerDetailsService:CustomerdetailsService){
+  constructor(private route: Router, private activeRouter: ActivatedRoute,
+    private store: Store<AppState>, private spinnerService: SpinnerService, private customerDetailsService: CustomerdetailsService) {
     this.customerDetails$ = this.store.pipe(select(getCustomerDetail));
   }
 
   ngOnDestroy(): void {
-   this.subscription.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {
 
-    this.subscription=this.customerDetails$.subscribe((data) => {
+    this.subscription = this.customerDetails$.subscribe((data) => {
       this.customerId = data?.customer._id; // Update the component's state with the new value
       console.log('Customer ID:', this.customerId); // Optional: Log the value
     });
 
-    this.isLoading$=this.spinnerService.loading$;
+    this.isLoading$ = this.spinnerService.loading$;
     this.spinnerService.showLoading();
     this.errorState = { hasError: false, message: '' };
+
     this.customerService.getAllCustomers().pipe(
       finalize(() => this.spinnerService.hideLoading())
     ).subscribe({
@@ -66,43 +69,74 @@ export class CustomersDashBoardComponent implements OnInit,OnDestroy{
     });
   }
 
-  planNow(customer:CustomerdetailsInterface,_id:any) {
+  planNow(customer: CustomerdetailsInterface, _id: any) {
     console.log();
 
     if (_id != null) {
 
-      this.route.navigate(['customerDashboard/',_id]);
-    //  this.store.dispatch(setCustomerId({_id}));
-     console.log("customer is is set",_id);
-     console.log(customer);
+      this.route.navigate(['customerDashboard/', _id]);
+      //  this.store.dispatch(setCustomerId({_id}));
+      console.log("customer is is set", _id);
+      console.log(customer);
 
-     this.store.dispatch(
-      setCustomer({
-        customer: {
-          _id: customer._id,
-          name: customer.name,
-          age: customer.age,
-          address: customer.address,
-          accomadation: customer.accomadation,
-          travelMode: customer.travelMode,
-          foodList: customer.foodList,
-          foodListOption: customer.foodListOption,
-          beverageList: customer.beverageList,
-          beverageListOption: customer.beverageListOption,
-          startDate: customer.startDate,
-          endDate: customer.endDate,
-          status:customer.status,
-          totalExpense:customer.totalExpense,
-          phonenum:customer.phonenum
-        },
-        loading: true,
-        error: null
-      })
-    );
-  }
+      this.store.dispatch(
+        setCustomer({
+          customer: {
+            _id: customer._id,
+            name: customer.name,
+            age: customer.age,
+            address: customer.address,
+            accomadation: customer.accomadation,
+            travelMode: customer.travelMode,
+            foodList: customer.foodList,
+            foodListOption: customer.foodListOption,
+            beverageList: customer.beverageList,
+            beverageListOption: customer.beverageListOption,
+            startDate: customer.startDate,
+            endDate: customer.endDate,
+            status: customer.status,
+            totalExpense: customer.totalExpense,
+            phonenum: customer.phonenum
+          },
+          loading: true,
+          error: null
+        })
+      );
+    }
   }
 
   completed(customer: CustomerdetailsInterface) {
     this.route.navigate(['']);
+  }
+
+  deleteCustomer(id: string) {
+    this.customerDetailsService.deleteCustomer(id).subscribe(
+      response => {
+        console.log('Customer deleted successfully', response);
+      },
+      error => {
+        console.error('Error deleting customer', error);
+      }
+    );
+    Swal.fire({
+      title: 'Action Successfully executed',
+      text: 'Customer deletion was successful',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
+
+    this.customerService.getAllCustomers().pipe(
+      finalize(() => this.spinnerService.hideLoading())
+    ).subscribe({
+      next: (data) => (this.customersList = data),
+      error: (err) => {
+        this.errorState = {
+          hasError: true,
+          message: err.message || 'An error occurred while fetching beverages.'
+        };
+        console.error('Error fetching beverages:', err);
+      }
+    });
+
   }
 }
