401baacc21a5f42358d163e1f07a206862e86f5f
Heshan Umayanga <38934645+heshanu@users.noreply.github.com>
2026-02-19 23:21:48 +0530

    implement i18n for login page (#171)

--- src/app/module/login/login.module.ts
+++ src/app/module/login/login.module.ts
@@ -4,11 +4,9 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule, FormsModule, FormGroup } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MaterialModule } from '../../shared/materialModule';
-
-
+import { TranslateModule } from '@ngx-translate/core';
 @NgModule({ declarations: [
         LoginComponent
 ], imports: [
@@ -16,7 +14,8 @@ import { MaterialModule } from '../../shared/materialModule';
         LoginRoutingModule,
         ReactiveFormsModule,
     FormsModule,
-        MaterialModule
+        MaterialModule,
+        TranslateModule
     ],
     providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class LoginModule {
